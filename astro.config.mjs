c1c287436d9e1b39599507885fe09ef516b3c8c8
timo <timopscherer@gmail.com>
2025-04-18 06:58:46 +0200

    stable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-<<<<<<< HEAD
-import svelte from '@astrojs/svelte';
-=======
 import tailwindcss from '@tailwindcss/vite';
->>>>>>> test
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://knuspermixx.github.io',
   base: 'astro-edit-starter',
-<<<<<<< HEAD
-  integrations: [svelte()],
-=======
 
   vite: {
     plugins: [tailwindcss()],
   },
->>>>>>> test
 });
