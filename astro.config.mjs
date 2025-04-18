0ef9d4e07fe282ddafc597bdf40479d55ca1861c
timo <timopscherer@gmail.com>
2025-04-18 06:43:56 +0200

    stable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://knuspermixx.github.io',
-    base: 'astro-edit-starter',
-  
+  site: 'https://knuspermixx.github.io',
+  base: 'astro-edit-starter',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
